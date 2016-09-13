f63bfe11322e74d870fa0dab66e1b773763a4607
mvaladezinst <mvaladez@instructure.com>
2016-09-13 12:50:40 -0600

    Create update_existing_enrollments.rb

---
+++ API/Enrollments/update_existing_enrollments/update_existing_enrollments.rb
@@ -0,0 +1,55 @@
+#=========================
+require 'csv'
+require 'typhoeus'
+
+#========================Replace these values=======================#
+
+# Either provide values at prompt, or provide values between '' marks and remove nil. 
+
+access_token = nil # place your basic token between '' marks
+domain = nil # place your Bridge domain between '' marks
+csv_file = nil # file path to CSV containing learner enrollment ID, score, and completion date
+
+#-------------------Do not edit below this line---------------------#
+
+unless access_token
+  puts 'What is your access token? Do not include "Authorization Basic", only the token'
+  access_token = gets.chomp
+end
+
+unless domain
+  puts 'What is your Bridge domain? Do not include "https://", or, ".bridgeapp.com"'
+  domain = gets.chomp
+end
+
+unless csv_file
+  puts 'Where is your enrollments update CSV located?, e.g., "/Users/Name/Desktop/Example.csv" '
+  csv_file = gets.chomp
+end
+
+unless File.exists?(csv_file)
+  raise "Error: can't locate the update CSV"
+end
+
+base_url = "https://#{domain}.bridgeapp.com/api/author"
+test_url = "#{base_url}/course_templates"
+
+
+# Make generic API call to test token, domain, and env.
+test = Typhoeus.get(test_url,
+                    headers: {'Content-Type'=>'application/json', 'accept'=>'application/json', 'Authorization'=>"Basic #{access_token}"}
+)
+
+unless test.code == 200
+  raise 'Error: The token or domain variables are not set correctly'
+end
+
+CSV.foreach(csv_file, {:headers => true}) do |row|
+  Typhoeus.patch("#{base_url}/enrollments/#{row['id']}",
+                params: { 'enrollments[0][score]' => row['score'], 'enrollments[0][completed_at]' => row['completed_at'] },
+                headers: {'Content-Type'=>'application/json', 'accept'=>'application/json', 'Authorization'=>"Basic #{access_token}"}
+  )
+  puts "#{row['id']}, #{row['score']}, #{row['completed_at']}"
+end
+
+puts 'Successfully updated enrollments'
